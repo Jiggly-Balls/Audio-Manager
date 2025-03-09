a5b3117851d662c00530144a2c9e73b1b85ee35b
Jiggly Balls <jigglyballs9000@gmail.com>
2025-03-09 22:14:15 +0530

    Added constants

---
+++ config.py
@@ -0,0 +1,7 @@
+from PySide6.QtGui import QFont
+
+FONT: QFont = QFont("Sanserif", 15)
+WINDOW_HEIGHT: int = 590
+WINDOW_WIDTH: int = 440
+TITLE_NAME: str = "Audio Manager"
+OPACITY_LEVEL: float = 0.9
